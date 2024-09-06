09bd354e35d1d8f3e30950a19c083d4da61e7366
Prajwal <imprjde7@gmail.com>
2024-09-07 01:33:45 +0530

    Filer button svg added

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -231,29 +231,29 @@
 //         <div className="flex gap-5">
 //           <AnimatePresence>
 //             {!showFilter && (
-//               <motion.div
-//                 onClick={() => setShowFilter(true)}
-//                 // initial={{ scale: 0 }}
-//                 // animate={{ scale: 1 }}
-//                 // transition={{ duration: 0.5, type: "spring", stiffness: 200 }}
-//                 className="fixed bottom-[72px] md:hidden right-4 z-40"
-//               >
-//                 <button className="flex border-[1.5px]    border-gray-600 space-x-1 items-center bg-gradient-to-r from-purple-700 to-gray-900 hover:from-gray-900 hover:to-purple-700 text-white font-semibold text-sm py-1.5 px-4 rounded-full ">
-//                   <span>Filters</span>
-//                   <span>
-//                     {" "}
-//                     <svg
-//                       xmlns="http://www.w3.org/2000/svg"
-//                       width="20px"
-//                       height="20px"
-//                       fill="currentColor"
-//                       viewBox="0 0 256 256"
-//                     >
-//                       <path d="M64,105V40a8,8,0,0,0-16,0v65a32,32,0,0,0,0,62v49a8,8,0,0,0,16,0V167a32,32,0,0,0,0-62Zm-8,47a16,16,0,1,1,16-16A16,16,0,0,1,56,152Zm80-95V40a8,8,0,0,0-16,0V57a32,32,0,0,0,0,62v97a8,8,0,0,0,16,0V119a32,32,0,0,0,0-62Zm-8,47a16,16,0,1,1,16-16A16,16,0,0,1,128,104Zm104,64a32.06,32.06,0,0,0-24-31V40a8,8,0,0,0-16,0v97a32,32,0,0,0,0,62v17a8,8,0,0,0,16,0V199A32.06,32.06,0,0,0,232,168Zm-32,16a16,16,0,1,1,16-16A16,16,0,0,1,200,184Z"></path>
-//                     </svg>
-//                   </span>
-//                 </button>
-//               </motion.div>
+// <motion.div
+//   onClick={() => setShowFilter(true)}
+//   // initial={{ scale: 0 }}
+//   // animate={{ scale: 1 }}
+//   // transition={{ duration: 0.5, type: "spring", stiffness: 200 }}
+//   className="fixed bottom-[72px] md:hidden right-4 z-40"
+// >
+//   <button className="flex border-[1.5px]    border-gray-600 space-x-1 items-center bg-gradient-to-r from-purple-700 to-gray-900 hover:from-gray-900 hover:to-purple-700 text-white font-semibold text-sm py-1.5 px-4 rounded-full ">
+//     <span>Filters</span>
+//     <span>
+//       {" "}
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         width="20px"
+//         height="20px"
+//         fill="currentColor"
+//         viewBox="0 0 256 256"
+//       >
+//         <path d="M64,105V40a8,8,0,0,0-16,0v65a32,32,0,0,0,0,62v49a8,8,0,0,0,16,0V167a32,32,0,0,0,0-62Zm-8,47a16,16,0,1,1,16-16A16,16,0,0,1,56,152Zm80-95V40a8,8,0,0,0-16,0V57a32,32,0,0,0,0,62v97a8,8,0,0,0,16,0V119a32,32,0,0,0,0-62Zm-8,47a16,16,0,1,1,16-16A16,16,0,0,1,128,104Zm104,64a32.06,32.06,0,0,0-24-31V40a8,8,0,0,0-16,0v97a32,32,0,0,0,0,62v17a8,8,0,0,0,16,0V199A32.06,32.06,0,0,0,232,168Zm-32,16a16,16,0,1,1,16-16A16,16,0,0,1,200,184Z"></path>
+//       </svg>
+//     </span>
+//   </button>
+// </motion.div>
 //             )}
 //           </AnimatePresence>
 
@@ -461,10 +461,25 @@ const Jobs = () => {
             {!showFilter && (
               <motion.div
                 onClick={() => setShowFilter(true)}
+                // initial={{ scale: 0 }}
+                // animate={{ scale: 1 }}
+                // transition={{ duration: 0.5, type: "spring", stiffness: 200 }}
                 className="floating-filter-button fixed bottom-[72px] md:hidden right-4 z-40"
               >
                 <button className="flex border-[1.5px]    border-gray-600 space-x-1 items-center bg-gradient-to-r from-purple-700 to-gray-900 hover:from-gray-900 hover:to-purple-700 text-white font-semibold text-sm py-1.5 px-4 rounded-full ">
                   <span>Filters</span>
+                  <span>
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20px"
+                      height="20px"
+                      fill="currentColor"
+                      viewBox="0 0 256 256"
+                    >
+                      <path d="M64,105V40a8,8,0,0,0-16,0v65a32,32,0,0,0,0,62v49a8,8,0,0,0,16,0V167a32,32,0,0,0,0-62Zm-8,47a16,16,0,1,1,16-16A16,16,0,0,1,56,152Zm80-95V40a8,8,0,0,0-16,0V57a32,32,0,0,0,0,62v97a8,8,0,0,0,16,0V119a32,32,0,0,0,0-62Zm-8,47a16,16,0,1,1,16-16A16,16,0,0,1,128,104Zm104,64a32.06,32.06,0,0,0-24-31V40a8,8,0,0,0-16,0v97a32,32,0,0,0,0,62v17a8,8,0,0,0,16,0V199A32.06,32.06,0,0,0,232,168Zm-32,16a16,16,0,1,1,16-16A16,16,0,0,1,200,184Z"></path>
+                    </svg>
+                  </span>
                 </button>
               </motion.div>
             )}
