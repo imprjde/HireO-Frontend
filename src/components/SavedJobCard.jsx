e0b9fbf8f1d140e16fee5374cdac6858287835dc
Prajwal Devadiga <111837734+imprjde@users.noreply.github.com>
2024-10-06 16:46:39 +0530

    Update SavedJobCard.jsx

--- src/components/SavedJobCard.jsx
+++ src/components/SavedJobCard.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
-import { BsBookmarkXFill } from "react-icons/bs";
 import { CiLocationOn } from "react-icons/ci";
 import { HiOfficeBuilding } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 import { HiBriefcase } from "react-icons/hi2";
-import { FaEye } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import axios from "axios";
@@ -39,62 +38,60 @@ export default function SavedJobCard({ job, setIsRemoving }) {
   };
 
   return (
-    <div>
-      <div
-        id="card"
-        className="bg-black shadow-sm md:shadow-md md:shadow-gray-100 border border-b-0 border-gray-700 shadow-gray-100 space-y-4 mx-4 p-4 rounded-md"
-      >
-        <div className="flex  items-center w-full  m-auto justify-between">
-          <div className="flex flex-col space-y-1  w-[70%]">
-            <span className="flex items-center space-x-1">
-              <p>
-                <HiBriefcase />
-              </p>
-              <p className="font-bold truncate">{job?.jobId?.title}</p>
-            </span>
-            <span className="flex items-center space-x-1">
-              <p>
-                <HiOfficeBuilding />
-              </p>
-              <p className="font-semibold truncate">
-                {job?.jobId?.company?.name}
-              </p>
-            </span>
-            <span className="flex items-center space-x-1">
-              <p>
-                <CiLocationOn />
-              </p>
+    <div className="relative">
+      <Link to={`/description/${job?.jobId?._id}`}>
+        <div
+          id="card"
+          className="bg-black pb-5 shadow-sm md:shadow-md md:shadow-gray-100 border border-b-0 border-gray-700 shadow-gray-100 space-y-4 mx-4 p-4 rounded-md"
+        >
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+              removeSavedJob(job?._id);
+            }}
+            className="absolute top-[-7px] bg-rose-600 rounded-full right-[7px]"
+          >
+            <IoClose size={20} className="text-white cursor-pointer" />
+          </div>
 
-              <p className="font-medium text-sm text-gray-200 truncate">
-                {job?.jobId?.location?.toLowerCase()?.includes("india")
-                  ? job?.jobId?.location
-                  : job?.jobId?.location + ", India"}
-              </p>
-            </span>
-          </div>
-          <div className="bg-rd-500 mb-7 justify-end ">
-            <img
-              src={job?.jobId?.company?.logo}
-              className="w-12 rounded-full"
-              alt="logo"
-            />
+          <div className="flex items-center w-full m-auto justify-between">
+            <div className="flex flex-col space-y-2 w-[70%]">
+              <span className="flex items-center space-x-1">
+                <p>
+                  <HiBriefcase />
+                </p>
+                <p className="font-bold truncate">{job?.jobId?.title}</p>
+              </span>
+              <span className="flex items-center space-x-1">
+                <p>
+                  <HiOfficeBuilding />
+                </p>
+                <p className="font-semibold truncate">
+                  {job?.jobId?.company?.name}
+                </p>
+              </span>
+              <span className="flex items-center space-x-1">
+                <p>
+                  <CiLocationOn />
+                </p>
+
+                <p className="font-medium text-sm text-gray-200 truncate">
+                  {job?.jobId?.location?.toLowerCase()?.includes("india")
+                    ? job?.jobId?.location
+                    : job?.jobId?.location + ", India"}
+                </p>
+              </span>
+            </div>
+            <div className="bg-rd-500 mb-7 justify-end">
+              <img
+                src={job?.jobId?.company?.logo}
+                className="w-12 rounded-full"
+                alt="logo"
+              />
+            </div>
           </div>
         </div>
-        <div id="button" className=" flex w-full justify-end space-x-4">
-          <Link
-            to={`/description/${job?.jobId?._id}`}
-            className=" text-sm text-sky-100 font-semibold"
-          >
-            <FaEye size={20} />
-          </Link>
-          <button
-            onClick={() => removeSavedJob(job?._id)}
-            className="text-rose-300"
-          >
-            <BsBookmarkXFill size={20} />
-          </button>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
